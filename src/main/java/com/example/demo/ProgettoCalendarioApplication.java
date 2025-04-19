b8bffe8bcf32addfc1a7814fd072f11d68cc5df7
Giulio Longo <giuliobrongo1996@gmail.com>
2025-04-19 16:00:02 +0200

    Primo commit

---
+++ src/main/java/com/example/demo/ProgettoCalendarioApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettoCalendarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgettoCalendarioApplication.class, args);
+	}
+
+}
